5e428c3d929928d651a709e4b658f241af967f62
Meki <105891543+MekiSoglu@users.noreply.github.com>
2024-10-15 11:20:34 +0300

    Add files via upload

---
+++ yazılımdogrulama/yazilimdogrulama/src/main/java/com/example/yazilimdogrulama/YazilimdogrulamaApplication.java
@@ -0,0 +1,13 @@
+package com.example.yazilimdogrulama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YazilimdogrulamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YazilimdogrulamaApplication.class, args);
+	}
+
+}
